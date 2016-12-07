ecdd89e37c497eca3e78c790843144cc62dc752b
zoudike <850592231@qq.com>
2016-12-07 23:10:25 +0800

    打开按钮文字注释

--- ETMX_1.2/ETMX_1.2/modules/TaskMainController/View/CustomeBtn2.m
+++ ETMX_1.2/ETMX_1.2/modules/TaskMainController/View/CustomeBtn2.m
@@ -14,14 +14,14 @@
 
 -(void)layoutSubviews{
    [super layoutSubviews];
-//    CGRect rect = self.frame;
-//    CGRect tempImageViewRect = self.imageView.frame;
-//    CGRect tempLabelRect = self.titleLabel.frame;
-//    CGFloat width = tempImageViewRect.size.width>tempLabelRect.size.width?tempImageViewRect.size.width:tempLabelRect.size.width;
-//    CGFloat height = tempLabelRect.size.height + tempImageViewRect.size.height;
-//    self.frame = CGRectMake(rect.origin.x, rect.origin.y,width+5 , height +ImageViewAndLabelMargin);
-//    self.imageView.frame = CGRectMake(0, 0, 32, 32);
-//    self.titleLabel.frame = CGRectMake(0, 32, width+5,height - 32);
+    CGRect rect = self.frame;
+    CGRect tempImageViewRect = self.imageView.frame;
+    CGRect tempLabelRect = self.titleLabel.frame;
+    CGFloat width = tempImageViewRect.size.width>tempLabelRect.size.width?tempImageViewRect.size.width:tempLabelRect.size.width;
+    CGFloat height = tempLabelRect.size.height + tempImageViewRect.size.height;
+    self.frame = CGRectMake(rect.origin.x, rect.origin.y,width+5 , height +ImageViewAndLabelMargin);
+    self.imageView.frame = CGRectMake(0, 0, 32, 32);
+    self.titleLabel.frame = CGRectMake(0, 32, width+5,height - 32);
 }
 
 @end

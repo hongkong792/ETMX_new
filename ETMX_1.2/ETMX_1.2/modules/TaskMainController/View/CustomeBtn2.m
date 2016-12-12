c7a83172abf8772c8be463b7dafcdb910b697b9c
zoudike <850592231@qq.com>
2016-12-12 10:53:47 +0800

    处理图片变形问题

--- ETMX_1.2/ETMX_1.2/modules/TaskMainController/View/CustomeBtn2.m
+++ ETMX_1.2/ETMX_1.2/modules/TaskMainController/View/CustomeBtn2.m
@@ -14,8 +14,9 @@
     [super layoutSubviews];
     CGRect rect = self.frame;
     self.frame = CGRectMake(rect.origin.x, rect.origin.y, 40, 60);
-    self.imageView.frame = CGRectMake(0, 0, 32, 32);
-    self.titleLabel.frame = CGRectMake(0, 32, 40, 60-32);
+    CGRect tempImageFrame = self.imageView.frame;
+    self.imageView.frame = CGRectMake(0, 0, tempImageFrame.size.width,tempImageFrame.size.height);
+    self.titleLabel.frame = CGRectMake(0, 40, 40, 60-40);
 }
     
 
